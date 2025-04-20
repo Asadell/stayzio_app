301c487c35e811eaf24ef5ec63493429826c0f3c
Asadell <m.satrioasadell@gmail.com>
2025-04-21 01:13:25 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stayzio_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/stayzio_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.stayzio_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
